38ec84791589238fe6296e6da71cc9fd9f8a30ff
kshirsagarnaresh <kshirsagarnaresh@gmail.com>
2022-06-27 09:23:27 -0400

    Initial code commit

---
+++ AdapterSDK/Controllers/FileController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdapterSDK.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FileController : ControllerBase
+    {
+        [HttpGet]
+        public string GetFileList(string dirName)
+        {
+            return $"List of files from directory {dirName}";
+        }
+    }
+}
---
+++ AdapterSDKClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using NSwagDemo.Services;
+using System;
+
+namespace AdapterSDKClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileClient obj = new FileClient();
+            obj.GetFileListAsync("DirectoryName");
+
+            WeatherForecastClient objClient = new WeatherForecastClient();
+            objClient.GetCoolestStateAsync();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
